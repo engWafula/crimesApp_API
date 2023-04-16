fdad76555815cdd0c16113d4d4b17fe93ab9028f
engWafula <wafulaallan5@gmail.com>
2023-04-16 14:25:12 +0300

    settingup everything

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required:true
+  },
+  crimes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Crimes',
+    },
+  ],
+  forensics:[
+       
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Forensics',
+          },
+       
+  ] 
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model("User",userSchema)
