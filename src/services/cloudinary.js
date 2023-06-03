3f64caee40b02b52acae14293f05af27cecd012b
engWafula <wafulaallan5@gmail.com>
2023-06-03 11:34:58 +0300

    fixing

---
+++ src/services/cloudinary.js
@@ -0,0 +1,23 @@
+import  cloudinary from  'cloudinary' ;
+
+export const Cloudinary = {
+    upload: async (image) => {
+        // return new Promise((resolve, reject) => {
+        const res = await cloudinary.v2.uploader.upload(image, {
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+            cloud_name: process.env.CLOUDINARY_NAME,
+            folder: 'spare-parts/',
+        });
+        return res.secure_url;
+    },
+    delete: async (publicId) => {
+        const res = await cloudinary.v2.uploader.destroy(publicId, {
+            //@ts-ignore
+          api_key: process.env.CLOUDINARY_API_KEY,
+          api_secret: process.env.CLOUDINARY_API_SECRET,
+          cloud_name: process.env.CLOUDINARY_NAME,
+        });
+        return res.result === 'ok';
+      },
+};
